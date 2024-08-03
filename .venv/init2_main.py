f653d6acddf25ef31d0ce0074c4b01dcfc10e1aa
flying_bucket <2311067@mail.nankai.edu.cn>
2024-08-03 18:49:42 +0800

    divide the loop

--- .venv/init2_main.py
+++ .venv/init2_main.py
@@ -4,14 +4,28 @@ from init2_run_p2 import run_p2
 from tqdm import trange
 import json
 
+def run_part(start, end, current_space, part_number):
+    for j in trange(start, end):
+        i = start - 1
+        current_space = run_p2(i, j, current_space)
+    for i in trange(start, end):
+        j = start
+        current_space = run_p2(i, j, current_space)
+    with open(rf'I:\AAA\dev3\data\search_space{part_number}.txt', 'w') as file:
+        json.dump(current_space, file)
+    return current_space
+
 if __name__ == '__main__':
     multiprocessing.freeze_support()
+    
     current_space=run_p1()
-    for j in trange (1,6):
-        i=0
-        current_space=run_p2(i,j,current_space)
-    for i in trange (1,6):
-        j=5
-        current_space=run_p2(i,j,current_space)
-    with open(r'I:\AAA\dev3\data\search_space.txt', 'w') as file:
-            json.dump(current_space, file)
+    for part_number in range(1, 10):
+        current_space = run_part(part_number, part_number + 1, current_space, part_number)
+
+        
+
+
+    
+    
+    
+
